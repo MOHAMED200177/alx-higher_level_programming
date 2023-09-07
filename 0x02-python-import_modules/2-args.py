87bd102ee791464cd8d6f0e5758267fbf9592570
MOHAMED <mohamedelafandy593@gmail.com>
2023-09-07 20:37:23 +0300

    'modules'

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
+    from sys import argv
+    count = len(argv) - 1
+    print("{:d} {:s}{:s}"
+          .format
+          (
+            count,
+            'argument' if count == 1 else 'arguments',
+            '.' if count == 0 else ':'
+            ))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
+        print("{:d}: {:s}".format(i + 1, argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     sum = 0
-    count = len(argv) - 1
-    if count == 0:
-        print("0")
-    else:
-        sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print(sum)
 
-    for i in range(1, count + 1):
-        sum += int(argv[i])
-
-    print("{:d}".format(sum))
