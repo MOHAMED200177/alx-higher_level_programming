22466255a6e793b92e3862558284daa8e2566ed3
MOHAMED <mohamedelafandy593@gmail.com>
2023-09-11 17:16:12 +0300

    'maxNum'

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newList = []
+    for i in my_list:
+        if (my_list[i] % 2 == 0)
+            return ("{:d} is divisible by 2".format(i))
+        else:
+            return ("{:d} is not divisible by 2".format(i))
+
+
