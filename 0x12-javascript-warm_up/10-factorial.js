c6496084c2650814797d21c7ede2b1cea0e616c4
MOHAMED <mohamedelafandy593@gmail.com>
2023-12-12 04:37:33 +0200

    'javaccript'

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,6 +5,6 @@ function Factorialize (num) {
   if (Number.isNaN(num) || num === 1) {
     return 1;
   }
-  return Factorialize (num - 1) * num;
+  return Factorialize(num - 1) * num;
 }
 console.log(Factorialize(argv));
