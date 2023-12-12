9dea37d3e74cf3ae0e72514a4f059e0c920bd395
MOHAMED <mohamedelafandy593@gmail.com>
2023-12-12 03:18:20 +0200

    'javaccript'

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log(process.argv[2] === undefined ? 'No argument' : process.argv[2]);
+if (process.argv.length >= 4) {
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+
+  console.log(`${arg1} is ${arg2}`);
+} else {
+  console.log("Please provide at least two arguments.");
+}
