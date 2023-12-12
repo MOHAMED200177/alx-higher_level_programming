cf690b137444faf8e93b57f1542dc9323243fb84
MOHAMED <mohamedelafandy593@gmail.com>
2023-12-12 04:36:05 +0200

    'javaccript'

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that computes and prints a factorial
 const argv = parseInt(process.argv[2]);
-function Factorialize(num) {
+function Factorialize (num) {
   if (Number.isNaN(num) || num === 1) {
     return 1;
   }
-  return Factorialize(num - 1) * num;
+  return Factorialize (num - 1) * num;
 }
 console.log(Factorialize(argv));
