d749048df2854ac621c16c6744dde44c104174b1
MOHAMED <mohamedelafandy593@gmail.com>
2023-12-12 04:04:20 +0200

    'javaccript'

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const frist = Number(process.argv[2]);
-if (isNaN(frist)){console.log('Missing number of occurrences');}
- else {for (let i = 0; i < frist; i++) {console.log('C is fun');}
+if (isNaN(frist))
+{
+  console.log('Missing number of occurrences');
+}
+else
+{
+  for (let i = 0; i < frist; i++) {
+    console.log('C is fun');
+  }
 }
