9d9adaf3d3d4f1cf5f0899170fd3d906f5cdc95a
MOHAMED <mohamedelafandy593@gmail.com>
2023-12-12 03:09:22 +0200

    'javaccript'

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 console.log(
-    process.argv[2] === undefined ? 'No argument' : process.argv[3] === undefined ? 'Argument found' : 'Arguments found'
+  process.argv[2] === undefined
+    ? "No argument"
+    : process.argv[3] === undefined
+    ? "Argument found"
+    : "Arguments found"
 );
