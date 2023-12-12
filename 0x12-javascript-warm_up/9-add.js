ff676ac8ab3dd0af4f1b8bc4270d36d746b219ad
MOHAMED <mohamedelafandy593@gmail.com>
2023-12-12 04:28:33 +0200

    'javaccript'

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-// script that prints the addition of 2 integers
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-function add(a, b) {
-  return a + b;
-}
-console.log(add(a, b));
+console.log(Number(process.argv[2]) + Number(process.argv[3]));
