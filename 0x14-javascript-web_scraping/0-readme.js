a7ec0f8948a3d70a7164dc489d5421fb0d8cea1a
MOHAMED <mohamedelafandy593@gmail.com>
2024-01-31 18:28:17 +0200

    'readfile'

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
-const fs = require("fs");
-
-fs.readFile(process.argv[2], "utf8", function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+// read arg file
+const fs = require('fs');
+fs.readFile('' + process.argv[2], 'utf8', (error, data) => {
+  if (!error) {
+    console.log(data);
+  } else console.log(error);
 });
