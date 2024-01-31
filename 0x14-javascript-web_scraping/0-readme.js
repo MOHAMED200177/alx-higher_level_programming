b059cb6209d0689b9d45ce4cf69d6b43eb28a55a
MOHAMED <mohamedelafandy593@gmail.com>
2024-01-31 18:19:54 +0200

    'javaScript'

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/user/bin/node
+// Write a script that reads and prints the content of a file.
+const { error } = require('console');
+const fs = require('fs');
+fs.readFile('' + process.argv[2], 'utf8', (err, data) => {
+    if(err) {
+        console.log(error);
+    } else {
+        console.log(data);
+    }
+});
