a09e2b7b81b2efdb8abd8f3aaec6ceb04bd483e4
MOHAMED <mohamedelafandy593@gmail.com>
2024-01-31 20:04:52 +0200

    'readfile'

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,7 @@ const fs = require('fs');
 fs.readFile('' + process.argv[2], 'utf8', (error, data) => {
   if (error) {
     console.log(error);
-  } else console.log(data);
+  } else {
+    console.log(data);
+  }
 });
