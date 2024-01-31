6048009d7faf32b4110f1b3ae38b5a20f82a4c7f
MOHAMED <mohamedelafandy593@gmail.com>
2024-01-31 20:12:17 +0200

    'readfile'

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-// Script to read and print the content of a file
-
-const fs = require("fs");
-
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 3) {
-  console.error("Usage: ./script.js <file_path>");
-  process.exit(1); // Exit with a non-zero status code to indicate an error
-}
-
-const filePath = process.argv[2];
-
-// Read the file content in utf-8
-fs.readFile(filePath, "utf8", (error, data) => {
+// read arg file
+const fs = require('fs');
+fs.readFile('' + process.argv[2], 'utf8', (error, data) => {
   if (error) {
-    console.error(error); // Print the error object
+    console.log(error);
   } else {
-    console.log(data); // Print the file content
+    console.log(data);
   }
 });
